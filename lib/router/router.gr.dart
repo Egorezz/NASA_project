4662051adbe209950a2aa35f045fe843efd9c5dc
Egorezz <2771716@gmail.com>
2025-12-13 00:12:54 +0300

    FolderArchitecture

--- lib/router/router.gr.dart
+++ lib/router/router.gr.dart
@@ -9,83 +9,22 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i2;
-import 'package:flutter/material.dart' as _i3;
-import 'package:nasa_app/main.dart' as _i1;
-
-/// generated route for
-/// [_i1.CardsListScreen]
-class CardsListRoute extends _i2.PageRouteInfo<CardsListRouteArgs> {
-  CardsListRoute({
-    _i3.Key? key,
-    required String title,
-    required String description,
-    List<_i2.PageRouteInfo>? children,
-  }) : super(
-         CardsListRoute.name,
-         args: CardsListRouteArgs(
-           key: key,
-           title: title,
-           description: description,
-         ),
-         initialChildren: children,
-       );
-
-  static const String name = 'CardsListRoute';
-
-  static _i2.PageInfo page = _i2.PageInfo(
-    name,
-    builder: (data) {
-      final args = data.argsAs<CardsListRouteArgs>();
-      return _i1.CardsListScreen(
-        key: args.key,
-        title: args.title,
-        description: args.description,
-      );
-    },
-  );
-}
-
-class CardsListRouteArgs {
-  const CardsListRouteArgs({
-    this.key,
-    required this.title,
-    required this.description,
-  });
-
-  final _i3.Key? key;
-
-  final String title;
-
-  final String description;
-
-  @override
-  String toString() {
-    return 'CardsListRouteArgs{key: $key, title: $title, description: $description}';
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-    if (other is! CardsListRouteArgs) return false;
-    return key == other.key &&
-        title == other.title &&
-        description == other.description;
-  }
-
-  @override
-  int get hashCode => key.hashCode ^ title.hashCode ^ description.hashCode;
-}
+import 'package:auto_route/auto_route.dart' as _i5;
+import 'package:flutter/material.dart' as _i6;
+import 'package:nasa_app/features/home/screens/home_screen.dart' as _i1;
+import 'package:nasa_app/features/main/screens/main_screen.dart' as _i2;
+import 'package:nasa_app/features/planets/screens/planets_screen.dart' as _i3;
+import 'package:nasa_app/features/settings/screens/settings_screen.dart' as _i4;
 
 /// generated route for
 /// [_i1.HomeScreen]
-class HomeRoute extends _i2.PageRouteInfo<void> {
-  const HomeRoute({List<_i2.PageRouteInfo>? children})
+class HomeRoute extends _i5.PageRouteInfo<void> {
+  const HomeRoute({List<_i5.PageRouteInfo>? children})
     : super(HomeRoute.name, initialChildren: children);
 
   static const String name = 'HomeRoute';
 
-  static _i2.PageInfo page = _i2.PageInfo(
+  static _i5.PageInfo page = _i5.PageInfo(
     name,
     builder: (data) {
       return const _i1.HomeScreen();
@@ -94,48 +33,97 @@ class HomeRoute extends _i2.PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [_i1.MainScreen]
-class MainRoute extends _i2.PageRouteInfo<MainRouteArgs> {
-  MainRoute({
-    _i3.Key? key,
-    required _i3.ThemeData theme,
-    List<_i2.PageRouteInfo>? children,
-  }) : super(
-         MainRoute.name,
-         args: MainRouteArgs(key: key, theme: theme),
-         initialChildren: children,
-       );
+/// [_i2.MainScreen]
+class MainRoute extends _i5.PageRouteInfo<void> {
+  const MainRoute({List<_i5.PageRouteInfo>? children})
+    : super(MainRoute.name, initialChildren: children);
 
   static const String name = 'MainRoute';
 
-  static _i2.PageInfo page = _i2.PageInfo(
+  static _i5.PageInfo page = _i5.PageInfo(
     name,
     builder: (data) {
-      final args = data.argsAs<MainRouteArgs>();
-      return _i1.MainScreen(key: args.key, theme: args.theme);
+      return const _i2.MainScreen();
     },
   );
 }
 
-class MainRouteArgs {
-  const MainRouteArgs({this.key, required this.theme});
+/// generated route for
+/// [_i3.PlanetsScreen]
+class PlanetsRoute extends _i5.PageRouteInfo<void> {
+  const PlanetsRoute({List<_i5.PageRouteInfo>? children})
+    : super(PlanetsRoute.name, initialChildren: children);
 
-  final _i3.Key? key;
+  static const String name = 'PlanetsRoute';
 
-  final _i3.ThemeData theme;
+  static _i5.PageInfo page = _i5.PageInfo(
+    name,
+    builder: (data) {
+      return const _i3.PlanetsScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [_i4.SettingsScreen]
+class SettingsRoute extends _i5.PageRouteInfo<SettingsRouteArgs> {
+  SettingsRoute({
+    _i6.Key? key,
+    required String title,
+    required String description,
+    List<_i5.PageRouteInfo>? children,
+  }) : super(
+         SettingsRoute.name,
+         args: SettingsRouteArgs(
+           key: key,
+           title: title,
+           description: description,
+         ),
+         initialChildren: children,
+       );
+
+  static const String name = 'SettingsRoute';
+
+  static _i5.PageInfo page = _i5.PageInfo(
+    name,
+    builder: (data) {
+      final args = data.argsAs<SettingsRouteArgs>();
+      return _i4.SettingsScreen(
+        key: args.key,
+        title: args.title,
+        description: args.description,
+      );
+    },
+  );
+}
+
+class SettingsRouteArgs {
+  const SettingsRouteArgs({
+    this.key,
+    required this.title,
+    required this.description,
+  });
+
+  final _i6.Key? key;
+
+  final String title;
+
+  final String description;
 
   @override
   String toString() {
-    return 'MainRouteArgs{key: $key, theme: $theme}';
+    return 'SettingsRouteArgs{key: $key, title: $title, description: $description}';
   }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-    if (other is! MainRouteArgs) return false;
-    return key == other.key && theme == other.theme;
+    if (other is! SettingsRouteArgs) return false;
+    return key == other.key &&
+        title == other.title &&
+        description == other.description;
   }
 
   @override
-  int get hashCode => key.hashCode ^ theme.hashCode;
+  int get hashCode => key.hashCode ^ title.hashCode ^ description.hashCode;
 }
